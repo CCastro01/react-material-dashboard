532a6d535da0a5759169a58a80665714ebe7eb6b
Christian Castro <alvescastro00cca13@gmail.com>
2020-04-29 10:22:39 -0300

    Add Tarefas

--- src/layouts/Main/components/Topbar/Topbar.js
+++ src/layouts/Main/components/Topbar/Topbar.js
@@ -7,6 +7,7 @@ import { AppBar, Toolbar, Badge, Hidden, IconButton } from '@material-ui/core';
 import MenuIcon from '@material-ui/icons/Menu';
 import NotificationsIcon from '@material-ui/icons/NotificationsOutlined';
 import InputIcon from '@material-ui/icons/Input';
+import {withRouter} from 'react-router-dom'
 
 const useStyles = makeStyles(theme => ({
   root: {
@@ -26,6 +27,12 @@ const Topbar = props => {
   const classes = useStyles();
 
   const [notifications] = useState([]);
+
+  const logout = () =>{ 
+    localStorage.removeItem('email_usuario_logado')
+    props.history.push('/login')
+  }
+
 
   return (
     <AppBar
@@ -51,6 +58,7 @@ const Topbar = props => {
             </Badge>
           </IconButton>
           <IconButton
+            onClick={logout}
             className={classes.signOutButton}
             color="inherit"
           >
@@ -75,4 +83,4 @@ Topbar.propTypes = {
   onSidebarOpen: PropTypes.func
 };
 
-export default Topbar;
+export default withRouter(Topbar);
--- src/views/SignIn/SignIn.js
+++ src/views/SignIn/SignIn.js
@@ -129,54 +129,15 @@ const SignIn = props => {
   const { history } = props;
 
   const classes = useStyles();
+  const [email, setEmail] = useState();
 
-  const [formState, setFormState] = useState({
-    isValid: false,
-    values: {},
-    touched: {},
-    errors: {}
-  });
-
-  useEffect(() => {
-    const errors = validate(formState.values, schema);
-
-    setFormState(formState => ({
-      ...formState,
-      isValid: errors ? false : true,
-      errors: errors || {}
-    }));
-  }, [formState.values]);
-
-  const handleBack = () => {
-    history.goBack();
-  };
-
-  const handleChange = event => {
-    event.persist();
-
-    setFormState(formState => ({
-      ...formState,
-      values: {
-        ...formState.values,
-        [event.target.name]:
-          event.target.type === 'checkbox'
-            ? event.target.checked
-            : event.target.value
-      },
-      touched: {
-        ...formState.touched,
-        [event.target.name]: true
-      }
-    }));
-  };
 
   const handleSignIn = event => {
     event.preventDefault();
+    localStorage.setItem("email_usuario_logado", email)
     history.push('/');
   };
 
-  const hasError = field =>
-    formState.touched[field] && formState.errors[field] ? true : false;
 
   return (
     <div className={classes.root}>
@@ -223,9 +184,7 @@ const SignIn = props => {
         >
           <div className={classes.content}>
             <div className={classes.contentHeader}>
-              <IconButton onClick={handleBack}>
-                <ArrowBackIcon />
-              </IconButton>
+
             </div>
             <div className={classes.contentBody}>
               <form
@@ -236,64 +195,41 @@ const SignIn = props => {
                   className={classes.title}
                   variant="h2"
                 >
-                  Sign in
+                  Login
                 </Typography>
                 <Typography
                   color="textSecondary"
                   gutterBottom
                 >
-                  Sign in with social media
                 </Typography>
                 <Grid
                   className={classes.socialButtons}
                   container
                   spacing={2}
                 >
-                  <Grid item>
-                    <Button
-                      color="primary"
-                      onClick={handleSignIn}
-                      size="large"
-                      variant="contained"
-                    >
-                      <FacebookIcon className={classes.socialIcon} />
-                      Login with Facebook
-                    </Button>
-                  </Grid>
-                  <Grid item>
-                    <Button
-                      onClick={handleSignIn}
-                      size="large"
-                      variant="contained"
-                    >
-                      <GoogleIcon className={classes.socialIcon} />
-                      Login with Google
-                    </Button>
-                  </Grid>
                 </Grid>
+
                 <Typography
                   align="center"
                   className={classes.sugestion}
                   color="textSecondary"
                   variant="body1"
                 >
-                  or login with email address
+                      Entre com seu endereço de email
+
                 </Typography>
                 <TextField
                   className={classes.textField}
-                  error={hasError('email')}
                   fullWidth
-                  helperText={
-                    hasError('email') ? formState.errors.email[0] : null
-                  }
                   label="Email address"
                   name="email"
-                  onChange={handleChange}
-                  type="text"
-                  value={formState.values.email || ''}
+                  type="email"
+                  value={email}
+                  onChange = { e => setEmail(e.target.value) }
                   variant="outlined"
                 />
-                <TextField
+
+                {/* <TextField
                   className={classes.textField}
                   error={hasError('password')}
                   fullWidth
@@ -306,19 +242,19 @@ const SignIn = props => {
                   type="password"
                   value={formState.values.password || ''}
                   variant="outlined"
-                />
+                /> */}
+
                 <Button
                   className={classes.signInButton}
                   color="primary"
-                  disabled={!formState.isValid}
                   fullWidth
                   size="large"
                   type="submit"
                   variant="contained"
                 >
-                  Sign in now
+                  Entrar
                 </Button>
-                <Typography
+                {/* <Typography
                   color="textSecondary"
                   variant="body1"
                 >
@@ -330,7 +266,7 @@ const SignIn = props => {
                   >
                     Sign up
                   </Link>
-                </Typography>
+                </Typography> */}
               </form>
             </div>
           </div>
